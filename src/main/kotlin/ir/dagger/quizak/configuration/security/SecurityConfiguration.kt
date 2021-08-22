8be4ed38b69c147ae2fa88d7e0b8c000f522fda6
Iman Ahrari <iahrari@pm.me>
2021-08-22 12:26:54 +0430

    Fixed remember me in login page to throw 500, after restarting server. Fix #7

--- src/main/kotlin/ir/dagger/quizak/configuration/security/SecurityConfiguration.kt
+++ src/main/kotlin/ir/dagger/quizak/configuration/security/SecurityConfiguration.kt
@@ -1,6 +1,8 @@
 package ir.dagger.quizak.configuration.security
 
 import ir.dagger.quizak.services.auth.ApplicationUserService
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.context.ApplicationContext
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider
@@ -47,5 +49,12 @@ class SecurityConfiguration(
 
     override fun configure(auth: AuthenticationManagerBuilder) {
         auth.authenticationProvider(daoAuthenticationProvider())
+    }
+
+    @Autowired
+    override fun setApplicationContext(context: ApplicationContext) {
+        super.setApplicationContext(context)
+        val globalAuthBuilder = context.getBean(AuthenticationManagerBuilder::class.java)
+        globalAuthBuilder.userDetailsService(applicationUserService)
     }
 }
