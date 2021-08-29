362bceb332208fe8cccec2bc2eae94d8bb789d45
Iman Ahrari <iahrari@pm.me>
2021-08-29 20:26:27 +0430

    Fixed private quizzes was searchable publicly. Fix #30

--- src/main/kotlin/ir/dagger/quizak/db/repostiory/QuizRepository.kt
+++ src/main/kotlin/ir/dagger/quizak/db/repostiory/QuizRepository.kt
@@ -5,7 +5,7 @@ import org.springframework.data.jpa.repository.JpaRepository
 import org.springframework.data.jpa.repository.Query
 
 interface QuizRepository: JpaRepository<Quiz, String>{
-    @Query("select q from Quiz q where (q.createdBy.id=?1 or q.classId.isPublicForAnyoneToUse=true or exists(select r from RegisterClass r where r.classI=q.classId and r.user.id=?1 and r.accepted=true)) and (q.name like %?2% or q.description like %?3%)")
+    @Query("select q from Quiz q where (q.classId.isPublicForAnyoneToUse=true or exists(select r from RegisterClass r where r.classI=q.classId and r.user.id=?1 and r.accepted=true)) and (q.createdBy.id=?1 or q.isPrivate=false) and (q.name like %?2% or q.description like %?3%)")
     fun findByNameContainingOrDescriptionContaining(
         userId: String?,
         name: String,
